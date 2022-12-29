f114ad1b140f43fbc1a533c8c0e9ba4eef9b4707
HappyMan17 <sebasgrisales16@gmail.com>
2022-12-29 13:47:42 -0500

    Conection with db and first part of user

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-import express from 'express'
+import express from 'express';
 
 const port = 3000;
 
 const app = express();
 app.use(express.json());
+
+app.get('/mande', (req, res) => {
+    res.send("API response");
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}` );
